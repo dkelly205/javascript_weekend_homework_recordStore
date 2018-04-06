bd1d19b99170575fb1f896a7e9da796d2d54e9ab
daniel kelly <danieljkelly91@gmail.com>
2018-04-06 11:08:07 +0100

    adds Record class and tests the properties

---
+++ specs/record_spec.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var Record = require('../record.js');
+
+describe('Record', function(){
+
+  var record;
+
+  beforeEach(function(){
+    record = new Record("Coldplay", "Yellow", "Rock", 6.99);
+  })
+
+  it('should have an artist', function(){
+    assert.strictEqual(record.artist, "Coldplay");
+  })
+
+  it('should have a title', function(){
+    assert.strictEqual(record.title, "Yellow");
+  })
+
+  it('should have a genre', function(){
+    assert.strictEqual(record.genre, "Rock");
+  })
+
+  it('should have a price', function(){
+    assert.strictEqual(record.price, 6.99);
+  })
+
+})
